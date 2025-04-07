91794fe91a3213f87c07a96d35a8d3200fd5a5a7
tacurry0 <alb.takarei@gmail.com>
2025-04-07 18:53:12 +0900

    Add files via upload

---
+++ jstest/sw.js
@@ -0,0 +1,27 @@
+
+const cacheName = 'football-app-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
