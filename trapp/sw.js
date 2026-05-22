9dbb9a8a47202676a5a1826d91f8045a04492d66
tacurry0 <alb.takarei@gmail.com>
2026-05-22 13:19:22 +0900

    Add files via upload

--- trapp/sw.js
+++ trapp/sw.js
@@ -1,20 +1,20 @@
 
-const cacheName = 'football-app-v14';
+const cacheName = 'football-app-v21';
 const assetsToCache = [
   './',
   './index.html',
   './style.css',
-  './script.js',
-  './scoreboard.html',
-  './scoreboard-display.html',
-  './scoreboard.css',
-  './scoreboard.js',
+  './script.js?v=20260522f',
   './vendor/html2canvas.min.js',
   './schedule/schedule.js',
   './schedule/schedule.json',
   './schedule/niigata_2024_schedule.json',
   './club_emblems.json',
+  './data/albirex_niigata_matches.json',
   './data/results.json',
+  './icons/100l.png',
+  './icons/j1.png',
+  './icons/j2.png',
   './icons/img_club_gunma.png',
   './manifest.json',
   './icons/icon-192.png',
@@ -25,7 +25,15 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(assetsToCache);
-    })
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
@@ -36,5 +44,3 @@ self.addEventListener('fetch', e => {
     })
   );
 });
-
-navigator.serviceWorker.register('/2025football_program/jstest/sw.js')
