7f9b5f61a4e84833f7d576766b58bdb01e7eb6ad
tacurry0 <alb.takarei@gmail.com>
2026-09-15 19:45:20 +0900

    Polish home match cards while preserving club identity and intro

--- trapp/sw.js
+++ trapp/sw.js
@@ -1,5 +1,5 @@
 
-const cacheName = 'football-app-v92-restore-ui';
+const cacheName = 'football-app-v93-home-polish';
 const assetsToCache = [
   "./",
   "./index.html",
@@ -10,6 +10,7 @@ const assetsToCache = [
   "./ui-v6.js",
   "./ui-v6.css",
   "./features.css",
+  "./home.css",
   "./features-model.js",
   "./features.js",
   "./analysis-data.js",
